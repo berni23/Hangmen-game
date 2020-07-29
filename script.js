0fb60e1e9902a3168c9eb6614461247ce136ebf7
Glitch (hello-webpage) <none>
2020-07-29 13:34:00 +0000

    👦☃️ Checkpoint ./script.js:18108482/5:18612461/151:19578617/648 ./index.html:18612461/16:19578617/9

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
+var alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
 
+console.log(alphabet);
 
+let gameLetters = document.querySelector(".game-letters")
 
+alphabet.forEach(letter => {
+  var newLetter = document.createElement("li");
+  console.log(letter);
+  newLetter.innerHTML = letter.toUpperCase();
+  gameLetters.appendChild(newLetter);
+});
 
